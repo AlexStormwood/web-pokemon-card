fac7733c7fef40d563cdb19ffe12bbfc79c7f24c
Alex <alex.holder@bigfootds.com>
2025-03-30 20:48:18 +1100

    feat: card rotates to mouse on hover

--- src/components/CardRenderer/CardRenderer.tsx
+++ src/components/CardRenderer/CardRenderer.tsx
@@ -2,14 +2,64 @@ import "./CardRenderer.css";
 
 import testingImage from "../../assets/cPK_10_005040_00_BIPPA_C_M_en_US_UT.png";
 import testingArt from "../../assets/cPK_10_005040_00_BIPPA_C_L_ILL.png";
+import { useEffect, useRef, useState } from "react";
 
 export const themes = {
 	pocket: {},
 };
 
 export default function CardRenderer() {
+
+	const refContainer = useRef<HTMLElement>(null);
+	const [dimensions, setDimensions] = useState({
+		width: 0, height: 0
+	});
+
+
+
+	useEffect(() => {
+		if (refContainer.current) {
+			setDimensions({
+				width: refContainer.current.offsetWidth,
+				height: refContainer.current.offsetHeight
+			})
+		}
+	}, [])
+
+	let [x, setX] = useState("0");
+	let [y, setY] = useState("0");
+
+	const rotateToMouse = (event: React.MouseEvent) => {
+		const baseX = event.clientX;
+		const baseY = event.clientY;
+		const middleX = dimensions.width / 2;
+		const middleY = dimensions.height / 2;
+		const offsetX = ((baseX - middleX) / middleX) * 45;
+		const offsetY = ((baseY - middleY) / middleY) * 45;
+
+		setX( -1 * offsetY + "deg");
+		setY(offsetX + "deg");
+	}
+
+	const resetRotationAfterDelay = () => {
+		setTimeout(() => {
+			setX("0deg");
+			setY("0deg");
+		}, 1000);
+	}
+
+
 	return (
-		<section className="cardRenderer">
+		<section 
+			className="cardRenderer" 
+			ref={refContainer}
+			onMouseMove={rotateToMouse}
+			onMouseOut={resetRotationAfterDelay}
+			style={{
+				'--active-card-rotateY': y,
+				'--active-card-rotateX': x,
+			} as React.CSSProperties}
+		>
 			<div className="cardContent">
 				<div className="cardHeader">
 					<p className="cardName">Bidoof</p>
