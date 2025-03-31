12a0427da200c8de7db343f45677463304f22f9b
Alex <alex.holder@bigfootds.com>
2025-03-31 14:57:46 +1100

    feat: more card scaling work, move data from css into react

--- src/components/CardRenderer/CardRenderer.tsx
+++ src/components/CardRenderer/CardRenderer.tsx
@@ -10,6 +10,11 @@ export const themes = {
 
 export default function CardRenderer({cardScale}: {cardScale: string}) {
 
+	let [borderThickness, setBorderThickness] = useState(1);
+	let [borderColourHex, setBorderColourHex] = useState("#dedfdf");
+	let [cornerRoundness, setCornerRoundness] = useState(1);
+
+
 	const refContainer = useRef<HTMLElement>(null);
 	const [dimensions, setDimensions] = useState({
 		width: 0, height: 0
@@ -26,6 +31,10 @@ export default function CardRenderer({cardScale}: {cardScale: string}) {
 				width: refContainer.current!.offsetWidth,
 				height: refContainer.current!.offsetHeight
 			});
+
+			console.log(refContainer.current!.offsetWidth, refContainer.current!.offsetHeight)
+
+			setBorderThickness((refContainer.current!.offsetHeight / 100) * 6)
 		});
 
 		resizeObserver.observe(refContainer.current);
@@ -58,6 +67,11 @@ export default function CardRenderer({cardScale}: {cardScale: string}) {
 		}, 1000);
 	}
 
+	// useEffect(() => {
+	// 	// nothing
+	// 	console.log("Border thickness changed");
+	// }, [borderThickness])
+
 
 	return (
 		<section 
@@ -71,24 +85,130 @@ export default function CardRenderer({cardScale}: {cardScale: string}) {
 				height:`${cardScale}dvh`
 			} as React.CSSProperties}
 		>
-			<div className="cardContent">
-				<div className="cardHeader">
-					<p className="cardName">Bidoof</p>
+			<div className="cardContent" style={{
+				position: "absolute",
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				flexDirection: "column"
+			}}>
+
+
+
+
+				<div className="cardHeader" style={{
+					position: "relative",
+					width: `${dimensions.width - (borderThickness)}px`,
+					marginTop: `${(borderThickness / 2)}px`,
+					marginLeft: `${(borderThickness / 2)}px`,
+					height: `${dimensions.height / 16.5}px`,
+					zIndex: `10`,
+					backgroundColor: "rgba(254, 167, 255, 0.43)"
+				}}>
+					<p className="cardName" style={{
+						position: "absolute",
+						textAlign: "left",
+						lineHeight: `100%`,
+						fontSize: `${dimensions.height / 3}%`,
+						fontWeight: `900`,
+						margin: `0`,
+						marginTop: `0.6%`,
+						marginLeft: `15%`,
+						letterSpacing: `-${dimensions.width / 4000}rem`
+					}}>
+						Bidoof
+					</p>
 				</div>
-				<div className="cardArtbox">
-					<img className="cardArtImage" src={testingArt} />
+
+
+
+
+				<div className="cardArtbox" style={{
+					position: "relative",
+					backgroundColor: "rgba(243, 245, 39, 0.43)",
+					width: `${dimensions.width - (borderThickness)}px`,
+					marginLeft: `${(borderThickness / 2)}px`,
+					height: `${dimensions.height / 2.39}px`,
+					display: "flex",
+					justifyContent: "center"
+				}}>
+					<div className="cardArtFrame" style={{
+						marginTop: `${(borderThickness / 10)}px`,
+						
+					}}>
+						<img className="cardArtImage" src={testingArt} style={{
+							width: `92%`,
+							aspectRatio: `1106/696`
+						}} />
+					</div>
 				</div>
-				<div className="cardBorder">
+
+				<div className="cardActions" style={{
+					position: "relative",
+					backgroundColor: "rgba(167, 236, 255, 0.43)",
+					width: `${dimensions.width - (borderThickness)}px`,
+					marginLeft: `${(borderThickness / 2)}px`,
+					height: `${dimensions.height / 2.95}px`,
+					display: "flex",
+					justifyContent: "center"
+				}}>
+
+
+				</div>
+
+				<div className="cardInfo" style={{
+					position: "relative",
+					backgroundColor: "rgba(255, 119, 25, 0.43)",
+					width: `${dimensions.width - (borderThickness)}px`,
+					marginLeft: `${(borderThickness / 2)}px`,
+					marginBottom: `${(borderThickness / 2)}px`,
+					flexGrow: "1",
+					display: "flex",
+					justifyContent: "center"
+				}}>
+
+
+				</div>
+
+
+				<div className="cardBorder" style={{
+					zIndex: "-1",
+					width: `${dimensions.width}px`,
+					height: `${dimensions.height}px`,
+					borderRadius: `${cornerRoundness}rem`,
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}  >
 					<svg
 						className="cardHollow"
 						xmlns="http://www.w3.org/2000/svg"
+						style={{
+							borderRadius: `${cornerRoundness}rem`,
+							width: `100%`,
+							height: `100%`,
+						}}
 					>
-						<rect />
+						<rect style={{
+							'strokeWidth': `${borderThickness +"px"}`,
+							width: `${dimensions.width}px`,
+							height: `${dimensions.height}px`,
+							backgroundColor: "red",
+							fill: "none",
+							stroke: borderColourHex
+						} as React.CSSProperties} />
 					</svg>
 				</div>
 			</div>
-			<div className="debugHelpers" style={{height: `${cardScale}dvh`}}>
-				<img src={testingImage} />
+			<div className="debugHelpers" style={{
+				height: `${dimensions.height}px`
+				}}>
+				<img src={testingImage} style={{
+					zIndex: "-10",
+					opacity: "0.3",
+					position: "relative",
+					height: "100%"
+				}} />
 			</div>
 		</section>
 	);
